a9c670ad2245e24d2fadd1508a7c01a6058bf310
yut831 <airman515@gmail.com>
2012-07-16 11:16:10 +0900

    例外処理/tokenファイルのパスを明示

--- twCommand/Program.cs
+++ twCommand/Program.cs
@@ -7,32 +7,46 @@ namespace twitCmd
 {
     class Program
     {
-        const string tokenfile = "token";
+        static string tokenfile = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\token";
 
         static void Main(string[] args)
         {
-            var consumer = new Consumer("Xq4JnC92AR0yoLS11VclWA", "urZb3nDEhyFDqDaK7Cjc9mRA3IVe3PtWB5STlzVI8");
-            AccessToken accesstoken;
-            if (!System.IO.File.Exists("token"))
+            try
             {
-                var reqtoken = consumer.ObtainUnauthorizedRequestToken("https://api.twitter.com/oauth/request_token", "http://twitter.com/");
-                System.Diagnostics.Process.Start(Consumer.BuildUserAuthorizationURL("https://api.twitter.com/oauth/authorize", reqtoken));
-                Console.Write("PINコードを入力>");
-                accesstoken = consumer.RequestAccessToken(Console.ReadLine(), reqtoken, "https://api.twitter.com/oauth/access_token", "http://twitter.com/");
+                if (args.Length != 0)
+                {
+                    var consumer = new Consumer("Xq4JnC92AR0yoLS11VclWA", "urZb3nDEhyFDqDaK7Cjc9mRA3IVe3PtWB5STlzVI8");
+                    AccessToken accesstoken;
+                    if (!System.IO.File.Exists(tokenfile))
+                    {
+                        var reqtoken = consumer.ObtainUnauthorizedRequestToken("https://api.twitter.com/oauth/request_token", "http://twitter.com/");
+                        System.Diagnostics.Process.Start(Consumer.BuildUserAuthorizationURL("https://api.twitter.com/oauth/authorize", reqtoken));
+                        Console.Write("PINコードを入力>");
+                        accesstoken = consumer.RequestAccessToken(Console.ReadLine(), reqtoken, "https://api.twitter.com/oauth/access_token", "http://twitter.com/");
 
-                using (var sw = new StreamWriter(tokenfile))
-                    new XmlSerializer(typeof(Tokens)).Serialize(sw, new Tokens() { TokenValue = accesstoken.TokenValue, TokenSecret = accesstoken.TokenSecret });
+                        using (var sw = new StreamWriter(tokenfile))
+                            new XmlSerializer(typeof(Tokens)).Serialize(sw, new Tokens() { TokenValue = accesstoken.TokenValue, TokenSecret = accesstoken.TokenSecret });
+                    }
+                    else
+                    {
+                        Tokens tokens;
+                        using (var sr = new StreamReader(tokenfile))
+                            tokens = new XmlSerializer(typeof(Tokens)).Deserialize(sr) as Tokens;
+                        accesstoken = new AccessToken(tokens.TokenValue, tokens.TokenSecret);
+                    }
+
+                    System.Net.HttpWebResponse resp = consumer.AccessProtectedResource(accesstoken, "https://api.twitter.com/1/statuses/update.json", "POST", "http://twitter.com/",
+                            new Parameter[] { new Parameter("status", args[0]) });
+                }
+                else
+                {
+                    Console.WriteLine("ツイート内容を指定してください");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                Tokens tokens;
-                using (var sr = new StreamReader(tokenfile))
-                    tokens = new XmlSerializer(typeof(Tokens)).Deserialize(sr) as Tokens;
-                accesstoken = new AccessToken(tokens.TokenValue, tokens.TokenSecret);
+                Console.WriteLine(ex.Message);
             }
-
-            System.Net.HttpWebResponse resp = consumer.AccessProtectedResource(accesstoken, "https://api.twitter.com/1/statuses/update.json", "POST", "http://twitter.com/",
-                    new Parameter[] { new Parameter("status", args[0]) });
         }
     }
 
